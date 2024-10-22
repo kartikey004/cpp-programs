389b64f63054af8a277542814ed8b15c3f740c16
kartikey004 <kartikeym004@gmail.com>
2024-10-23 01:05:25 +0530

    Practice

---
+++ first.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define nl "\n"
+using namespace std;
+
+void solve()
+{
+
+    cout << "MY FIRST PROGRAM" << endl;
+   
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // int t;
+    // cin >> t;
+    // for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
